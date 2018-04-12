2eb682306a2d1585e5bb8ce30c93e1fbe034643b
MaxMEllon <maxmellon1994@gmail.com>
2018-04-12 21:51:52 +0900

    Refactoring

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 const isFunc = x => typeof x === 'function'
+const each = Symbol('each')
 
-export default class ArrayLikeSet extends Set {
+class ArrayLikeSet extends Set {
+  static from (array) {
+    return new ArrayLikeSet(array)
+  }
+
+  [each](behavior) {
+    let index = 0
+    for (const [i, _] of super.entries()) {
+      behavior(i, index++)
+    }
+  }
+
   map(func) {
     if (!isFunc(func)) throw new TypeError('Expected function')
-    let index = 0
     const result = new ArrayLikeSet()
-    for (const [i, _] of super.entries()) {
-      result.add(func(i, index))
-    }
+    this[each]((val, idx) => result.add(func(val, idx)))
     return result
   }
 
   filter(func) {
     if (!isFunc(func)) throw new TypeError('Expected function')
-    let index = 0
     const result = new ArrayLikeSet()
-    for (const [i, _] of super.entries()) {
-      const r = func(i, index)
-      if (r) result.add(i)
-    }
+    this[each]((val, idx) => {
+      const r = func(val, idx)
+      if (r) result.add(val)
+    })
     return result
   }
 }
